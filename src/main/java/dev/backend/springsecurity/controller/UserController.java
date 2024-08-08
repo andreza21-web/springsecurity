3c005e826dc1da8d9bd9c43e1359e5d641c9a682
Andreza Pessoa <andreza.21pessoa@gmail.com>
2024-08-08 18:06:53 -0300

    add funcionalidade criar novo usuario

---
+++ src/main/java/dev/backend/springsecurity/controller/UserController.java
@@ -0,0 +1,57 @@
+package dev.backend.springsecurity.controller;
+
+import dev.backend.springsecurity.controller.dto.CreateUserDto;
+import dev.backend.springsecurity.entities.Role;
+import dev.backend.springsecurity.entities.User;
+import dev.backend.springsecurity.repository.RoleRepository;
+import dev.backend.springsecurity.repository.UserRepository;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Set;
+
+@RestController
+public class UserController {
+
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    public UserController(UserRepository userRepository,
+                          RoleRepository roleRepository,
+                          BCryptPasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Transactional
+    @PostMapping("/users")
+    public ResponseEntity<Void> newUser(@RequestBody CreateUserDto dto) {
+
+        //trocar para public depois
+        var roleUser = roleRepository.findByName(Role.Values.BASIC.name());
+
+        var userFromDb = userRepository.findByUsername(dto.username());
+        if (userFromDb.isPresent()) {
+            throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+
+        var user = new User();
+        user.setUsername(dto.username());
+        user.setPassword(passwordEncoder.encode(dto.password()));
+        user.setRoles(Set.of(roleUser));
+
+        userRepository.save(user);
+
+        return ResponseEntity.ok().build();
+    }
+
+}
